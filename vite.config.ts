9e35a8707d3e9ec1588d8cbb30f9a3c3dcea6ce7
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-12 11:55:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DigiSolutionsCV/',
   // GitHub Pages වලදී path ප්‍රශ්න ඇති නොවීමට මෙම base කොටස අනිවාර්ය වේ.
   // මෙමගින් වෙබ් අඩවිය ඕනෑම sub-directory එකක වුවද නිවැරදිව රන් වීමට මග පාදයි.
   base: './',
